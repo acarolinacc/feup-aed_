7790654cd638665775c020171f03e7cea4a97cc5
Rodrigo-up09 <rodriribeiro2004@gmail.com>
2023-10-28 20:53:28 +0100

    done

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Rodrigo/Desktop/program/aed/projeto aed/feup_aed1
+# Install script for directory: C:/Users/Rodrigo/Desktop/program/aed/projeto aed/feup-aed1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Rodrigo/Desktop/program/aed/projeto aed/feup_aed1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Rodrigo/Desktop/program/aed/projeto aed/feup-aed1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
